7fbf810754a1322673b7e4269ebdefc2c7977d29
chinnariSrinivasasai <chinnarichekravarthi@gmail.com>
2026-06-29 11:01:25 +0530

    clean cookies-2

--- downloader/views.py
+++ downloader/views.py
@@ -66,10 +66,12 @@ def home(request):
                 "format": "best[ext=mp4]/best",
                 "quiet": True,
                 "noplaylist": True,
-                "cookiefile": "clean_cookies.txt", 
+                "cookiefile": "clean_cookies.txt",
                 "http_headers": {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 Edg/149.0.0.0",
                     "Accept-Language": "en-US,en;q=0.9",
+                    "Origin": "https://www.youtube.com",
+                    "Referer": "https://www.youtube.com/",
                 }
             }
 
